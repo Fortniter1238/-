0ec190bc347f762c458683ff00531c7761b29ebc
Fortniter1238 <70965606+Fortniter1238@users.noreply.github.com>
2025-09-14 00:45:25 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+// Отдаём статические файлы из папки public
+app.use(express.static("public"));
+
+// Хранилище сообщений в памяти
+let messages = [];
+
+// Обработка подключений
+io.on("connection", (socket) => {
+  console.log("Пользователь подключился:", socket.id);
+
+  // Отправляем историю сообщений новому пользователю
+  socket.emit("init", messages);
+
+  // Получаем новое сообщение
+  socket.on("message", (msg) => {
+    const newMsg = {
+      text: msg.text,
+      user: msg.user || "Гость",
+      time: new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+    };
+    messages.push(newMsg);
+    io.emit("message", newMsg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Пользователь отключился:", socket.id);
+  });
+});
+
+// Render передаёт порт через переменную окружения
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
