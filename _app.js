23eea6511203153b915f9858b4a435b95900c37d
Fortniter1238 <70965606+Fortniter1238@users.noreply.github.com>
2025-09-14 00:10:18 +0300

    Delete _app.js

--- _app.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import "../styles.css";
-
-export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    fetch("/api/socket"); // инициализация сервера
-  }, []);
-  return <Component {...pageProps} />;
-}
